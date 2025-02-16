5fce9baf9f1aa61faa93b5ebf9d9a4ac113c92ce
Luis <lcampanogoldar@gmail.com>
2025-02-16 18:36:57 +0100

    Hice accesible Flask desde fuera

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def recibir_datos():
     return jsonify({"mensaje": "Video generado correctamente", "ruta_video": resultado_video})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
